3d7165c4fa6ed99be7b1de8b343800f9cad14ebd
phamhoang3345 <phamhoang3k5@gmail.com>
2026-09-16 21:23:46 +0700

    Create auth.js

---
+++ auth.js
@@ -0,0 +1,25 @@
+function login(username, password) {
+    // Kiểm tra username rỗng
+    if (!username) {
+        return false;
+    }
+
+    // Kiểm tra password rỗng
+    if (!password) {
+        return false;
+    }
+
+    // Tài khoản bị khóa
+    if (username === "locked") {
+        return false;
+    }
+
+    // Kiểm tra username và password
+    if (username === "admin" && password === "123") {
+        return true;
+    }
+
+    return false;
+}
+
+module.exports = { login };
